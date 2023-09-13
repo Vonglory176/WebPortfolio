48489e3e1f543a583098668f672845d0ae71dc28
Vonglory176 <SkylerGConley@gmail.com>
2023-09-13 11:19:49 -0700

    Now using HTML elements in project description

--- src/data/projectData.jsx
+++ src/data/projectData.jsx
@@ -4,16 +4,16 @@ export default [
         link:"https://vonglory176.github.io/ImageGenerator/",
         projectImages:[
             {
-                alt:"Image generator project image 1",
+                alt:"AI Image Generator project image 1",
                 image:"https://media.discordapp.net/attachments/1117279430281285632/1144375437066244277/image.png"
             },
             {
-                alt:"Image generator project image 2",
+                alt:"AI Image Generator project image 2",
                 image:"https://media.discordapp.net/attachments/1117279430281285632/1144378120556773558/image.png"
             }
         ],
-        title:"Image Generator",
-        description:"This project is exactly as it sounds, that being a small React application allowing you to use AI to easily create detailed artwork! Through the use of Horde-AI API services and a series of fetch calls, clicking the generate button will send whatever settings you've configured off to be processed and returned as an image of your design. The work itself was of course very heavily focused on fundamental React concepts (like useState and useEffect) and Asynchronous Fetch/API calls in particular, though also involved extensive utilization of React-Bootstrap for much of the UI. With the “Batch Size” feature being the latest addition to my program, I'm also planning on adding some basic Local Storage functions to allow easy reuse of previous prompts. I'd like a gallery feature of sorts as well, though this looks to require back-end work and is as such an idea on the shelf for now.",
+        title:"AI Image Generator",
+        description:[<p>This project is exactly as it sounds, that being a small React application allowing you to use AI to easily create detailed artwork!</p>, <p>Through the use of Horde-AI API services and a series of fetch calls, clicking the generate button will send whatever settings you've configured off to be processed and returned as an image of your design. The work itself was of course very heavily focused on fundamental React concepts (like useState and useEffect) and Asynchronous Fetch calls in particular, though also involved extensive utilization of React-Bootstrap for much of the UI. </p>, <p>With the “Batch Size” feature being the latest addition to my program, I'm also planning on adding some basic Local Storage functions to allow easy reuse of previous prompts. I'd like a gallery feature of sorts as well, though this looks to require back-end work and is as such an idea on the shelf for now.</p>],
         skillLogos:[
             "HTML","CSS","Sass","Bootstrap","Javascript","React","Vite"
             // {
@@ -38,16 +38,16 @@ export default [
         link:"https://vonglory176.github.io/Mock-ReactDoc-Page/",
         projectImages:[
             {
-                alt:"React Documentation project image 1",
+                alt:"React Documentation Page project image 1",
                 image:"https://media.discordapp.net/attachments/1117279430281285632/1119786085124411463/Screenshot_2023-06-17_172718.png"
             },
             {
-                alt:"React Documentation project image 2",
+                alt:"React Documentation Page project image 2",
                 image:"https://media.discordapp.net/attachments/1117279430281285632/1119786085376077894/Screenshot_2023-06-17_172904.png"
             }
         ],
-        title:"React.js Documentation",
-        description:"This is a little documentation page built to give the reader some information on the React Javascript library. In building it, most all attention was on media queries and bootstrap use to make the page's content completely responsive and scalable. It has 3 main viewports in which the navbar will shift around the screen, the mobile view in particular condensing it to a small hamburger menu. Each link within said navbar is connected to a certain section of the page's content, this also incorperating some basic JavaScript/jQuery to keep the navbar out of the way of headers. Note that the content images included were left on the larger side to better demonstrate this.",
+        title:"React.js Documentation Page",
+        description:[<p>This is a little documentation page built to give the reader some information on the React Javascript library.</p>, <p>In building it, most all attention was on media queries and bootstrap use to make the page's content completely responsive and scalable. It has 3 main viewports in which the navbar will shift around the screen, the mobile view in particular condensing it to a small hamburger menu. Each link within said navbar is connected to a certain section of the page's content, this also incorperating some basic JavaScript / jQuery to keep the navbar out of the way of headers.</p>, <p>Note that the content images included were left on the larger side to better demonstrate this.</p>],
         skillLogos:[
             "HTML","CSS","Bootstrap","Javascript","jQuery"
             // {
@@ -82,16 +82,16 @@ export default [
         link:"https://vonglory176.github.io/SpaceShip-Game/",
         projectImages:[
             {
-                alt:"Asteroid Gauntlet project image 1",
+                alt:"Asteroid Gauntlet Game project image 1",
                 image:"https://media.discordapp.net/attachments/1117279430281285632/1119796973562757180/sddsdfsdf.png"
             },
             {
-                alt:"Asteroid Gauntlet project image 2",
+                alt:"Asteroid Gauntlet Game project image 2",
                 image:"https://media.discordapp.net/attachments/1117279430281285632/1119796973176893582/sddsjkljkdfsdf.png"
             }
         ],
-        title:"Asteroid Gauntlet",
-        description:"This was a final project I'd made for my JavaScript class, the coding done entirely by myself and the art assets very kindly drawn for me by my friend Edva. It was my first project of this type and took me a few weeks to put together, but I think it came out pretty well! There are a couple small imperfections with things like hit detection, optimization and so on, but I'd gotten to what I could at the time and am very proud of the outcome. The game runs within an HTML Canvas element nested within a simple bootstrap frame, the game objects themselves drawn from a series of sprite-sheets and animated through frame-requests and event triggers.",
+        title:"Asteroid Gauntlet Game",
+        description:[<p>This was a final project I'd made for a college class in JavaScript. The coding was done entirely by myself and the art assets very kindly drawn for me by my friend Edva.</p>, <p>It was my first project of this type and it took me a few weeks to put together as such, but I think that in the end that it came out pretty well! There are a couple small imperfections with things like hit detection, optimization and so on, but I'd gotten to what I could at the time and am very proud of the outcome.</p>, <p>The game runs within an HTML Canvas element nested within a simple bootstrap frame, the game objects themselves drawn from a series of sprite-sheets and animated through frame-requests and event triggers.</p>],
         skillLogos:[
             "HTML","CSS","Bootstrap","Javascript","jQuery"
             // {
