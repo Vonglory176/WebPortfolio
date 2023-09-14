6471b79122cf5df6c8ed2433c2307da5a3fe5082
Vonglory176 <SkylerGConley@gmail.com>
2023-09-14 10:10:54 -0700

    React-Bootstrap carousel & github/project buttons

--- src/component/ProjectCard.jsx
+++ src/component/ProjectCard.jsx
@@ -1,13 +1,20 @@
 import React from 'react'
-import { useInView} from 'react-intersection-observer'
+import {useInView} from 'react-intersection-observer'
+import Carousel from 'react-bootstrap/Carousel';
 import SkillLogo from "./SkillLogo"
 
-export default function ProjectCard({position,link,projectImages,title,description,skillLogos}) {
+export default function ProjectCard({position,projectLink,githubLink,projectImages,title,description,skillLogos}) {
     const {ref, inView, entry} = useInView({threshold:0.7})
     // https://www.youtube.com/watch?v=r1auJEf9ISo
     // https://github.com/thebuilder/react-intersection-observer
     // https://dev.to/producthackers/intersection-observer-using-react-49ko
     // https://youtu.be/QD4GcZJObXg
+
+    const [index, setIndex] = React.useState(0);
+
+    const handleSelect = (selectedIndex) => {
+        setIndex(selectedIndex);
+    };
     
     //Determining tile shift direction
     const reverse = Boolean(position%2)
@@ -16,9 +23,11 @@ export default function ProjectCard({position,link,projectImages,title,descripti
     let projectImageElements = []
     for (let i=0;i<projectImages.length;i++) {
         {projectImageElements.push(
-            <div className={"carousel-item" + (i===0? " active":"")}>
+        // <div className={"carousel-item" + (i===0? " active":"")}>
+        <Carousel.Item key={projectImages[i].alt}>
             <img className="img-fluid" alt={projectImages[i].alt} src={projectImages[i].image}/>
-        </div>
+        </Carousel.Item>
+        // </div>
         )}
     }
 
@@ -36,33 +45,37 @@ export default function ProjectCard({position,link,projectImages,title,descripti
     return (
         <div className="project-card card col-11 col-md-9 col-lg-11 col-xl-10">
             {/* Carousel half */}
-            <a ref={ref} className={"project-imageLink row-12 col-lg-7 " + 
+            <div ref={ref} className={"project-imageLink row-12 col-lg-7 " + 
             (reverse? "animate-middle-left" + (inView? " animate-1" : "") : 
-            "animate-middle-left-reverse" + (inView? " animate-1-reverse" : ""))
-        } 
-            href={link}  target="blank_"> {/* animate-middle-left*/}
-                <span className="badge badge-pill bg-primary">Click to visit!</span>
-                <div className="carousel slide" data-bs-ride="carousel">
-                    <div className="carousel-inner">
-                        {projectImageElements}
-                    </div>
-                </div>
-            </a>
+            "animate-middle-left-reverse" + (inView? " animate-1-reverse" : ""))}>
+
+                {/* <div className='badgeContainer-div d-flex flex-column'>
+                    <a href={projectLink}  target="blank_" className="badge badge-pill bg-primary">Click for the Project!</a>
+                    <a href={githubLink}  target="blank_" className="badge badge-pill bg-warning text-black">Click for the Github!</a>
+                </div> */}
+
+                <Carousel key={"slide"} activeIndex={index} onSelect={handleSelect}>
+                    {projectImageElements}
+                </Carousel>
+
+            </div>
             
             {/* Content half */}
             <div ref={ref} className={"project-content col-12 col-lg-6 " + 
             (reverse? "animate-middle-right" + (inView? " animate-2" : "") : 
                       "animate-middle-right-reverse" + (inView? " animate-2-reverse" : ""))
-            }> {/* animate-middle-right*/}
+            }>
                 <h5 className="project-title">{title}</h5>
-                {/* <p className="project-description">{description}</p> */}
                 <div className="project-description">{description}</div>
-                {/* <hr/> */}
                 <div className="project-tools">
-                    <h5 className='m-0'>Tools used:</h5>
+                    <h5 className='m-0'>Tools:</h5>
                     {skillLogoElements}
                 </div>
                 <hr/>
+                <div className='d-flex justify-content-center gap-2'>
+                    <a href={projectLink}  target="blank_" className="btn btn-primary" role="button">View Project</a>
+                    <a href={githubLink}  target="blank_" className="btn btn-warning text-black" role="button">View Github Repo</a>
+                </div>
             </div>
         </div>
 
