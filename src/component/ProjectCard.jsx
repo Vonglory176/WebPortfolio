1b7c80b57fb45b5adb2a410a8851d5dcf72df68c
Vonglory176 <SkylerGConley@gmail.com>
2023-09-17 11:19:10 -0700

    First stab load optimization for project image

--- src/component/ProjectCard.jsx
+++ src/component/ProjectCard.jsx
@@ -3,7 +3,7 @@ import {useInView} from 'react-intersection-observer'
 import Carousel from 'react-bootstrap/Carousel';
 import SkillLogo from "./SkillLogo"
 
-export default function ProjectCard({position,projectLink,githubLink,projectImages,title,description,skillLogos}) {
+export default function ProjectCard({position,imagePosition,projectLink,githubLink,projectImages,title,description,skillLogos}) {
     const {ref, inView, entry} = useInView({threshold:0.7})
     // https://www.youtube.com/watch?v=r1auJEf9ISo
     // https://github.com/thebuilder/react-intersection-observer
@@ -11,6 +11,7 @@ export default function ProjectCard({position,projectLink,githubLink,projectImag
     // https://youtu.be/QD4GcZJObXg
 
     const [index, setIndex] = React.useState(0);
+    const [loadedImages, setLoadedImages] = React.useState([]);
 
     const handleSelect = (selectedIndex) => {
         setIndex(selectedIndex);
@@ -19,15 +20,43 @@ export default function ProjectCard({position,projectLink,githubLink,projectImag
     //Determining tile shift direction
     const reverse = Boolean(position%2)
 
-    //Generating carousel slides
+    //Monitoring image load completion
+    React.useEffect(() => {
+        let loadedImagesArray = [];
+        projectImages.forEach((image, index) => {
+            const newImage = new Image();
+            newImage.src = image.image;
+            newImage.onload = () => {
+                loadedImagesArray[index] = true;
+                setLoadedImages([...loadedImagesArray]);
+            };
+        });
+    }, [projectImages]);
+    
+    //Generating carousel slides for return
     let projectImageElements = []
     for (let i=0;i<projectImages.length;i++) {
         {projectImageElements.push(
-        // <div className={"carousel-item" + (i===0? " active":"")}>
-        <Carousel.Item key={projectImages[i].alt}>
-            <img className="img-fluid" alt={projectImages[i].alt} src={projectImages[i].image}/>
+        <Carousel.Item 
+        key={projectImages[i].alt}
+        style={{
+            backgroundImage:`url(${projectImages[i].imageSmall})`,
+            backgroundSize: 'cover'
+            }}>
+            
+            <img 
+            className="img-fluid" 
+            alt={projectImages[i].alt} 
+            src={projectImages[i].image} 
+            loading="lazy"
+            style={{
+                objectPosition:imagePosition, 
+                opacity: loadedImages[i] ? 1 : 0, //Only showing when fully loaded
+                transition: '200ms ease-in-out'
+
+            }} 
+            />
         </Carousel.Item>
-        // </div>
         )}
     }
 
