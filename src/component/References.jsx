a4f16118c97ec9a7ba7ac19e086a1a4e9124d363
Vonglory176 <SkylerGConley@gmail.com>
2023-10-19 16:10:52 -0700

    First implementation of References. Needs work

---
+++ src/component/References.jsx
@@ -0,0 +1,25 @@
+export default function References() {
+    return (
+    <section id="references-section">
+        <h1 className="references-title section-title shadow"><span>Kind Words</span> <span className="nbsp">&nbsp;</span> <span>From Cool People</span></h1>
+        <div className="references-div skillsContainer-div sectionContainer-div col-11 col-md-9"> {/*Make this a carosuel?*/}
+            <h5>Keith Rowley, My BATECH-289 Professor</h5>
+            <hr/>
+            <p>To whom it may concern,</p>
+            <p>Skyler Conley was a student in my Spring 2023 BATECH 289 Front End Web Development class. He was an outstanding student, and has a unique perspective on both the coding process and the visual design process. I met one-on-one with him weekly, and was constantly amazed at his enthusiasm for coding and by the fluency of his web development. Our meetings were exciting, honest, forthright, fun, and unpredictable. I never knew what cool new code he would throw at his projects and assignments. He taught me a lot of new processes and pushed my ideas of what is possible.</p>
+            <p>He was also a good listener, and was quite often inspired by my perspectives and feedback. I enjoyed the easy way we were able to toss ideas around, and loved seeing him work in front of my eyes in real time.</p>
+            <p>I must also toss in the fact that he earned perfect scores on every single assignment. In short, Skyler is a model student and, I believe, a model front end developer. His communication skills, enthusiasm for learning, and friendly personality will allow him to serve quite well within any web development team.</p>
+            <p>Feel free to contact me directly if you'd like to further discuss Skyler and his work.</p>
+            <p>Sincerely,</p>
+            <p>
+                Keith Rowley                    <br/>
+                Senior Adjunct                  <br/>
+                School of Business & Technology <br/>
+                Bellevue College                <br/>
+                k.rowley@bellevuecollege.edu    <br/>
+            </p>
+            <hr/>
+        </div>
+    </section>
+    )
+}
