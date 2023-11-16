a4703a78457b276dad3a9b4b959ffb2b67056aa5
Vonglory176 <SkylerGConley@gmail.com>
2023-11-16 12:19:32 -0800

    Added a new reference-div

--- src/component/References.jsx
+++ src/component/References.jsx
@@ -20,6 +20,20 @@ export default function References() {
             </p>
             <hr/>
         </div>
+        <div className="references-div skillsContainer-div sectionContainer-div col-11 col-md-9">
+            <h5>Lawrence Conley, My Disney SDET Internship Boss</h5>
+            <hr/>
+            <p>Skyler's 4.0 GPA and Phi Theta Kappa honors society membership won him a summer internship with the Global Quality Engineering team here at Disney Parks Technology Services. We needed a JavaScript-centric SDET with solid knowledge of the node.js framework, and he blew through our white-boarding session without a hiccup. His JavaScript skills are actually much more worthy of a senior level SDET than an entry level intern and we were very happy to bring him onboard! </p>
+            <p>During his time with us he helped us to write and re-factor step-definitions for our node.js test automation framework, but was most beneficial in helping to rewrite an in-house Chrome extension originally used for gathering data from our ALM database, and re-tooling it to work with our new qTest test case management tool. </p>
+            <p>The process was very complex and required plowing through tons of poorly commented spaghetti code, making sense of it all, and then methodically rebuilding all functions to work with the new database. His code easily passed review and was merged swiftly. His work on the tool actually unblocked the team! If we had the headcount available, we would have hired him as a cast member! He will be an excellent addition to any development team in need of a JavaScript pro!</p>
+            <p>
+                Lawrence Conley<br/>
+                SDET Lead<br/>
+                Walt Disney Parks and Resorts Online at Walt Disney Attractions Technology<br/>
+                lconley@mindspring.com<br/>
+            </p>
+            <hr/>
+        </div>
     </section>
     )
 }
