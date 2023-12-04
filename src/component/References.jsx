d708a3db9a790337c9d032042773f15a8a738a48
Vonglory176 <SkylerGConley@gmail.com>
2023-12-03 17:05:14 -0800

    Added a third reference, that being Kurt's

--- src/component/References.jsx
+++ src/component/References.jsx
@@ -2,6 +2,20 @@ export default function References() {
     return (
     <section id="references-section">
         <h1 className="references-title section-title shadow"><span>Kind Words</span> <span className="nbsp">&nbsp;</span> <span>From Cool People</span></h1>
+        <div className="references-div skillsContainer-div sectionContainer-div col-11 col-md-9">
+            <h5>Lawrence Conley, My Disney SDET Internship Boss</h5>
+            <hr/>
+            <p>Skyler's 4.0 GPA and Phi Theta Kappa honors society membership won him a summer internship with the Global Quality Engineering team here at Disney Parks Technology Services. We needed a JavaScript-centric SDET with solid knowledge of the node.js framework, and he blew through our white-boarding session without a hiccup. His JavaScript skills are actually much more worthy of a senior level SDET than an entry level intern and we were very happy to bring him onboard! </p>
+            <p>During his time with us he helped us to write and re-factor step-definitions for our node.js test automation framework, but was most beneficial in helping to rewrite an in-house Chrome extension originally used for gathering data from our ALM database, and re-tooling it to work with our new qTest test case management tool. </p>
+            <p>The process was very complex and required plowing through tons of poorly commented spaghetti code, making sense of it all, and then methodically rebuilding all functions to work with the new database. His code easily passed review and was merged swiftly. His work on the tool actually unblocked the team! If we had the headcount available, we would have hired him as a cast member! He will be an excellent addition to any development team in need of a JavaScript pro!</p>
+            <p>
+                Lawrence Conley<br/>
+                SDET Lead<br/>
+                Walt Disney Attractions Technology<br/>
+                lconley@mindspring.com<br/>
+            </p>
+            <hr/>
+        </div>
         <div className="references-div skillsContainer-div sectionContainer-div col-11 col-md-9"> {/*Make this a carosuel?*/}
             <h5>Keith Rowley, My BATECH-289 Professor</h5>
             <hr/>
@@ -21,16 +35,19 @@ export default function References() {
             <hr/>
         </div>
         <div className="references-div skillsContainer-div sectionContainer-div col-11 col-md-9">
-            <h5>Lawrence Conley, My Disney SDET Internship Boss</h5>
+            <h5>Kurt Friedrich, My PROG-209 Professor</h5>
             <hr/>
-            <p>Skyler's 4.0 GPA and Phi Theta Kappa honors society membership won him a summer internship with the Global Quality Engineering team here at Disney Parks Technology Services. We needed a JavaScript-centric SDET with solid knowledge of the node.js framework, and he blew through our white-boarding session without a hiccup. His JavaScript skills are actually much more worthy of a senior level SDET than an entry level intern and we were very happy to bring him onboard! </p>
-            <p>During his time with us he helped us to write and re-factor step-definitions for our node.js test automation framework, but was most beneficial in helping to rewrite an in-house Chrome extension originally used for gathering data from our ALM database, and re-tooling it to work with our new qTest test case management tool. </p>
-            <p>The process was very complex and required plowing through tons of poorly commented spaghetti code, making sense of it all, and then methodically rebuilding all functions to work with the new database. His code easily passed review and was merged swiftly. His work on the tool actually unblocked the team! If we had the headcount available, we would have hired him as a cast member! He will be an excellent addition to any development team in need of a JavaScript pro!</p>
+            <p>To whom it may concern,</p>
+            <p>Skyler Conley was a student of mine a web programming class at Bellevue College. Back in January of 2023 the course was called PROG209, it has since been renamed to DEV209.  It is a 2nd year course which focuses on writing a complete web app, both the client code and the server-side code. For the client, the course requires appropriate use of JavaScript objects and constructors, HTML, CSS, a SPA (Single Page Application) JavaScript framework, and making RESTful calls to the server to persist and retrieve data. The sever side code was done in a Virtual Machine hosting Linux with the Node.js server. Data was persisted to the Linux file system.  All sources were kept in GitHub. For a second project, Skyler was required to write a web-based game using the HTML Canvas features. </p>
+
+            <p>Skyler was a solid programmer, and he had no difficulty comprehending new material.  He received perfect scores on every homework and test. In addition, he always went well beyond the actually assignment, writing considerable extra code to make his solution to a problem much more eloquent and user friendly. His web app project and his Asteroid Gauntlet game project (see his resume), were substantially better than the average games from other students. </p>
+
+            <p>Because this was an online class, there was very little student to student interaction. However, what I observed in his interactions with me in class was a friendly, articulate, and responsible student, a real pleasure to have in my class.</p>
+            <p>Sincerely,</p>
             <p>
-                Lawrence Conley<br/>
-                SDET Lead<br/>
-                Walt Disney Attractions Technology<br/>
-                lconley@mindspring.com<br/>
+                Kurt Friedrich<br/>
+                Faculty at Bellevue College <br/>
+                kurt.friedrich@bellevuecollege.edu<br/>
             </p>
             <hr/>
         </div>
