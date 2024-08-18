da34405cb685a2dd44bd20d790c795c671a0a27f
Vonglory176 <SkylerGConley@gmail.com>
2024-08-17 19:35:59 -0700

    Removing old files for mass overhaul

--- src/component/SkillDropdownItem.jsx
+++
@@ -1,8 +0,0 @@
-export default function SkillDropdownItem({checked,content}) {
-    return (
-        <div className="skillDropdownItem-div">
-            <input className="form-check-input" type="checkbox" checked={checked} readOnly /> {/*style={{margin:"5px 0 0 6px"}}*/}
-            {content}
-        </div>
-    )
-}
