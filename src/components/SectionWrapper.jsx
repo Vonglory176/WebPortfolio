1b53c9e139edfeefd3b676e9964613703482d715
Vonglory176 <SkylerGConley@gmail.com>
2024-08-22 07:26:04 -0700

    Made headers sticky

--- src/components/SectionWrapper.jsx
+++ src/components/SectionWrapper.jsx
@@ -2,10 +2,10 @@ import React from 'react'
 
 const SectionWrapper = ({id, title, children}) => {
   return (
-    <section id={id} className='flex flex-col min-h-screen w-full'>
+    <section id={id} className='flex flex-col min-h-screen w-full'> {/* min-h-screen */}
 
         {/* Section Header */}
-        {title && <div className='bg-gray-800 bg-opacity-80'>
+        {title && <div className='bg-gray-800 bg-opacity-80 sticky top-0 z-9'>
             <h1 className='text-2xl font-bold text-center p-4 relative z-[11]'>{title}</h1>
         </div>}
 
