e109e7bfa9f26b8f7c04588a343a9a0f8cc972f4
Vonglory176 <SkylerGConley@gmail.com>
2024-08-22 14:25:59 -0700

    New file for project cards

---
+++ src/components/ProjectCard.jsx
@@ -0,0 +1,42 @@
+import React from 'react'
+import { FaExternalLinkAlt, FaGithub, FaImages, FaInfo, FaTools } from 'react-icons/fa'
+import TextCarosuel from './TextCarosuel'
+import { useSiteContext } from '../context/SiteContext'
+
+const ProjectCard = ({project, index}) => {
+    const { showModal } = useSiteContext()
+
+    return (
+        <div key={index} className='project-card bg-gray-800 rounded-lg overflow-hidden relative w-[343px] h-[195px]'>
+
+                
+            <img src={project.images[0]} alt={project.name} className='project-card-image w-full h-full object-cover' />
+
+            <div className='project-card-info flex flex-col items-center justify-between text-center gap-2 p-4 absolute bottom-0 bg-black bg-opacity-40 w-full h-full'>
+                
+                {/* Project Name and Description */}
+                <div className='flex flex-col items-center justify-center gap-2'>
+                    <h3 className='text-xl font-bold'>{project.name}</h3>
+                    <p>{project.description}</p>
+                </div>
+
+                {/* Skills */}
+                <TextCarosuel skills={project.skills} />
+
+                {/* Buttons */}
+                <div className='flex items-center justify-between gap-2 w-full'>
+                    <a href={project.link} target='_blank' className='bg-blue-600 text-white p-3 rounded-full'><FaExternalLinkAlt size={20} /></a>
+                    <a href={project.github} target='_blank' className='bg-blue-600 text-white p-3 rounded-full'><FaGithub size={20} /></a>
+                    <button onClick={() => showModal(project)} className='bg-blue-600 text-white p-3 rounded-full flex items-center gap-2'>
+                        <FaImages size={20} />
+                        {/* <FaTools size={20} /> */}
+                        {/* <FaInfo size={20} /> */}
+                    </button>
+                </div>
+            </div>
+
+        </div>
+    )
+}
+
+export default ProjectCard
