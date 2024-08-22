b810de0d0bea3e5651984ab2bc23598465042a05
Vonglory176 <SkylerGConley@gmail.com>
2024-08-22 14:22:55 -0700

    Added Context, a modal and a ToDo list

--- src/App.jsx
+++ src/App.jsx
@@ -5,15 +5,21 @@ import Projects from "./components/Projects"
 import Skills from "./components/Skills"
 import Contact from "./components/Contact"
 import References from "./components/References"
+import ProjectCardModal from "./components/ProjectCardModal"
+import { useSiteContext } from "./context/SiteContext"
 
 
 function App() {
+  const { modal } = useSiteContext()
+
   return (
     <div className="App">
       
       <main className="bg-gray-900 text-white">
 
+
         <Header />
+        {modal && <ProjectCardModal />}
 
         <Landing />
 
@@ -34,3 +40,14 @@ function App() {
 }
 
 export default App
+
+/*
+TODO:
+
+Hook-up contact form
+Fix button clicking while opacity is 0 in project card
+Make nav menu
+Get image preloading working
+Get resume download working
+
+*/
