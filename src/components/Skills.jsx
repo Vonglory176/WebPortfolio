8c21de80e0accb54da8427729e86896fadec77c4
Vonglory176 <SkylerGConley@gmail.com>
2024-08-22 07:25:19 -0700

    Added a basic skill container with category buttons

--- src/components/Skills.jsx
+++ src/components/Skills.jsx
@@ -1,13 +1,62 @@
-import React from 'react'
+import React, { useState } from 'react'
 import SectionWrapper from './SectionWrapper'
+import { skills } from '../assets/skills'
+import { FaCode, FaServer, FaTools } from 'react-icons/fa'
 
 const Skills = () => {
-  return (
-    <SectionWrapper id='skills' title='Skills'>
-        <div className='flex flex-col items-center justify-center'>
-        </div>
-    </SectionWrapper>
-  )
+    const [currentSkill, setCurrentSkill] = useState('frontend')
+
+    const handleSkillChange = (skill) => {
+        setCurrentSkill(skill)
+    }
+
+    // CHANGE UNSKILLS TO BE HIDDEN, NOT UNMOUNTED
+
+    return (
+        <SectionWrapper id='skills' title='Skills'>
+            <div className='flex flex-col flex-1 items-center p-4 justify-center gap-4'> {/* flex-1 h-full  */}
+
+                <div className="flex flex-col items-center gap-6 p-4 rounded-lg bg-gray-800 h-[630px] w-full max-w-[500px]">
+
+                    {/* Skill Selector */}
+                    <div className="grid grid-cols-3 gap-5 justify-around w-full">
+                        <button name='frontend' title='Frontend Skills' onClick={() => handleSkillChange('frontend')} className={`flex justify-center ${currentSkill === 'frontend' ? 'bg-blue-900' : 'bg-blue-700'} p-2 duration-200 rounded hover:bg-blue-800 items-center gap-2`}>
+                            <FaCode size={24} /> 
+                            <p className="hidden sm:inline-block">Frontend</p>
+                        </button>
+                        <button name='backend' title='Backend Skills' onClick={() => handleSkillChange('backend')} className={`flex justify-center ${currentSkill === 'backend' ? 'bg-blue-900' : 'bg-blue-700'} p-2 duration-200 rounded hover:bg-blue-800 items-center gap-2`}>
+                            <FaServer size={24} /> 
+                            <p className="hidden sm:inline-block">Backend</p>
+                        </button>
+                        {/* <button name='design' onClick={handleSkillChange} className='bg-blue-700 p-2 duration-200 rounded hover:bg-blue-800 flex items-center gap-2'>
+                            <FaPaintBrush /> Design
+                        </button> */}
+                        <button name='other' title='Other Skills' onClick={() => handleSkillChange('other')} className={`flex justify-center ${currentSkill === 'other' ? 'bg-blue-900' : 'bg-blue-700'} p-2 duration-200 rounded hover:bg-blue-800 flex items-center gap-2`}>
+                            <FaTools size={24} /> 
+                            <p className="hidden sm:inline-block">Other</p>
+                        </button>
+                    </div>
+
+                    <hr className='w-full border-gray-700' />
+
+                    {/* Skill Printing */}
+                    <div className="grid grid-cols-3 justify-between gap-5 w-full">
+                        {skills[currentSkill]?.map((skill, index) => (
+
+                            <div key={index} className="flex flex-col items-center gap-2" title={skill.description}>
+                                {skill.image && <skill.image size={32} />}
+                                <h3 className='text-center'>{skill.name}</h3>
+
+                                {/* <img src={skill?.image} alt={skill.name} className="w-10 h-10" /> */}
+                                {/* <p>{skill.description}</p> */}
+                            </div>
+                        ))}
+                    </div>
+
+                </div>
+            </div>
+        </SectionWrapper>
+    )
 }
 
 export default Skills
