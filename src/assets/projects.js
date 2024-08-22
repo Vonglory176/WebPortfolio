639b72aa486f245dda47e72ad0c09cb0ed8047d9
Vonglory176 <SkylerGConley@gmail.com>
2024-08-22 14:26:32 -0700

    New file for project data

---
+++ src/assets/projects.js
@@ -0,0 +1,15 @@
+import gymAppImage1 from '../assets/images/projects/gymApp/gym-app-1.png'
+import gymAppImage2 from '../assets/images/projects/gymApp/gym-app-2.png'
+export const projects = [
+    {
+        name: 'Gym App',
+        description: 'A website for building workout plans',
+        images: [
+            gymAppImage1,
+            gymAppImage2,
+        ],
+        link: 'https://www.example.com/project1',
+        github: 'https://www.example.com/project1',
+        skills: ['HTML', 'CSS', 'Tailwind', 'TypeScript', 'React', 'Vite'],
+    },
+]
