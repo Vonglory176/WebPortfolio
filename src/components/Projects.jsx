f630c834b56a57354427615181ec0bc0e8f490a9
Vonglory176 <SkylerGConley@gmail.com>
2024-08-22 14:24:30 -0700

    Added project printing functionality

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -1,10 +1,22 @@
 import React from 'react'
 import SectionWrapper from './SectionWrapper'
+import { projects } from '../assets/projects'
+import ProjectCard from './ProjectCard'
+// import { FaExternalLinkAlt, FaGithub, FaImages, FaInfo, FaTools } from 'react-icons/fa'
 
 const Projects = () => {
   return (
     <SectionWrapper id='projects' title='Projects'>
-        <div className='flex flex-1 flex-col p-4 items-center justify-center'>
+        <div className='flex flex-1 flex-col px-4 py-8 items-center justify-center'>
+
+          <div className='flex flex-col gap-4'>
+            {projects.map((project, index) => (
+
+              <ProjectCard project={project} index={index} />
+
+            ))}
+          </div>
+
         </div>
     </SectionWrapper>
   )
