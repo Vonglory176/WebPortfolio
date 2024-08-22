0e38a4d73dec51ad94be205ed2e116543fd6a5a6
Vonglory176 <SkylerGConley@gmail.com>
2024-08-22 14:23:53 -0700

    Changed h1 to h2

--- src/components/SectionWrapper.jsx
+++ src/components/SectionWrapper.jsx
@@ -5,8 +5,8 @@ const SectionWrapper = ({id, title, children}) => {
     <section id={id} className='flex flex-col min-h-screen w-full'> {/* min-h-screen */}
 
         {/* Section Header */}
-        {title && <div className='bg-gray-800 bg-opacity-80 sticky top-0 z-9'>
-            <h1 className='text-2xl font-bold text-center p-4 relative z-[11]'>{title}</h1>
+        {title && <div className='bg-gray-800 bg-opacity-80 sticky top-0 z-10'>
+            <h2 className='text-2xl font-bold text-center p-4 relative z-[11]'>{title}</h2>
         </div>}
 
         {/* Section Content */}
