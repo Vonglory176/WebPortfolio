c01b626454071e481165eadf4bf1fc159a0adc2e
Vonglory176 <SkylerGConley@gmail.com>
2024-08-23 11:27:21 -0700

    Updated image routes

--- src/assets/projects.js
+++ src/assets/projects.js
@@ -1,9 +1,11 @@
-import gymAppImage1 from '../assets/images/projects/gymApp/gym-app-1.png'
-import gymAppImage2 from '../assets/images/projects/gymApp/gym-app-2.png'
+import gymAppImage1 from '../assets/images/projects/gymApp/gym-app-1.webp'
+import gymAppImage2 from '../assets/images/projects/gymApp/gym-app-2.webp'
+import gymAppThumbnail from '../assets/images/projects/gymApp/gym-app-thumbnail.webp'
 export const projects = [
     {
         name: 'Gym App',
         description: 'A website for building workout plans',
+        thumbnail: gymAppThumbnail,
         images: [
             gymAppImage1,
             gymAppImage2,
