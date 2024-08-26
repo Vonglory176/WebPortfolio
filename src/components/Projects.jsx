93f5247a9a51c62e606ab0bd62fb7760a016630c
Vonglory176 <SkylerGConley@gmail.com>
2024-08-25 19:57:31 -0700

    Added a background images and tweaked z-index values

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -2,20 +2,35 @@ import React from 'react'
 import SectionWrapper from './SectionWrapper'
 import { projects } from '../assets/projects'
 import ProjectCard from './ProjectCard'
+import ProgressiveImage from 'react-progressive-graceful-image'
+import bgColorImg from '../assets/images/bg-colors-6.png'
 // import { FaExternalLinkAlt, FaGithub, FaImages, FaInfo, FaTools } from 'react-icons/fa'
 
 const Projects = () => {
   return (
     <SectionWrapper id='projects' title='Projects'>
-        <div className='flex flex-1 flex-col px-4 py-8 items-center justify-center'>
+        <div className='flex flex-1 flex-col px-4 py-8 items-center justify-center relative'>
 
-          <div className='flex flex-col gap-4'>
+          <div className='flex flex-col gap-4 z-10'>
             {projects.map((project, index) => (
 
               <ProjectCard project={project} index={index} key={index} />
 
             ))}
           </div>
+
+          {/* Background Image */}
+          <div className="background-image-container pointer-events-none">
+                <ProgressiveImage src={bgColorImg} placeholder={''}>
+                    {(src, loading) => (
+                        <img
+                            src={src}
+                            alt={""}
+                            className={`w-full h-full object-cover duration-300 ${loading ? 'opacity-0' : 'opacity-100'}`}
+                        />
+                    )}
+                </ProgressiveImage>
+            </div>
 
         </div>
     </SectionWrapper>
