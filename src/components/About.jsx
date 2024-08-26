e4435e58d39bffae86e38be7a6098a53b63ad2d4
Vonglory176 <SkylerGConley@gmail.com>
2024-08-25 19:55:00 -0700

    Changed max-widths and text formatting

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -1,38 +1,68 @@
 import React from 'react'
 import SectionWrapper from './SectionWrapper'
 import { FaCheckCircle, FaCodeBranch } from 'react-icons/fa'
+import bgColorImg from '../assets/images/bg-colors-1.png'
+import ProgressiveImage from 'react-progressive-graceful-image'
 
 const About = () => {
   return (
     <SectionWrapper id='about' title='About'>
-        <div className='flex flex-1 flex-col px-4 py-8 items-center justify-center gap-4'> {/* flex-1 h-full  */}
+        <div className='flex flex-1 flex-col px-4 py-8 items-center justify-center gap-4 bg-gray-800 bg-opacity-30 relative'> {/* flex-1 h-full  */}
 
-            <div className="flex flex-col items-center gap-4 p-4 rounded-lg bg-gray-800">
-                <p>Results-oriented and highly motivated full-stack developer with 3+ years of experience. Heavily focused on Web-Development with extensive experience using MERN, this including strong familiarity of Responsive Design, Best Practices and W3C Standards among others. I am self-motivated, a fast learner and am always eager to tackle a challenge to expand my skillset!</p>
+            <div className="content-wrapper max-w-[548px] mx-auto"> {/* 619px */}
 
-                <button className='bg-blue-700 px-4 py-2 rounded hover:bg-blue-800'>Download Resume</button>
-            </div>
+                <div className="flex flex-col items-center gap-4 px-4 py-6 rounded-lg bg-gray-800 z-10 sha">
+                    {/* <p>Results-oriented and highly motivated <span className='text-blue-500'>full-stack developer</span> with <span className='text-blue-500'>3+ years</span> of experience.</p>
+                    <p>Heavily focused on Web-Development with extensive experience working with the <span className='text-blue-500'>MERN tech stack</span>, this including strong familiarity of <span className='text-blue-500'>Responsive Design, Best Practices and W3C Standards</span> among others.</p>
+                    <p>I am self-motivated, a fast learner and am always eager to tackle a challenge to expand my skillset!</p> */}
 
-            <div className="flex justify-between w-full p-4">
+                    {/* <p>Results-oriented and highly motivated <span className='text-blue-500'>Full-Stack Developer</span> with <span className='text-blue-500'>+3 years</span> of experience.</p>
+                    <p>Focused on Web Development with extensive experience in the <span className='text-blue-500'>MERN tech stack</span>, including <span className='text-blue-500'>Responsive Design</span>, <span className='text-blue-500'>Best Practices</span>, and <span className='text-blue-500'>W3C Standards</span>.</p>
+                    <p><span className='text-blue-500'>Self-motivated</span>, <span className='text-blue-500'>fast learning</span> and always eager to tackle a challenge to expand my skillset!</p> */}
+                    
+                    <h3 className='text-center text-2xl font-bold w-full'>Meet Skyler</h3>
+                    <p>Results-oriented and highly motivated <span className='text-blue-500'>Full-Stack Developer</span> with <span className='text-blue-500'>3+ years</span> of experience in <span className='text-blue-500'>Web Development</span>, specializing in the <span className='text-blue-500'>MERN tech stack</span>.</p>
+                    <p>Proficient in <span className='text-blue-500'>Responsive Design</span>, <span className='text-blue-500'>Best Practices</span>, and <span className='text-blue-500'>W3C Standards</span>. <span className='text-blue-500'>Self-motivated</span>, <span className='text-blue-500'>fast learner</span>, and always eager to tackle new challenges to expand my skillset.</p>
 
-                <div className="flex flex-col items-center gap-2 text-center">
-                    <FaCheckCircle size={24} />
-                    <h3 className='text-lg font-bold'>+8</h3>
-                    <p>Projects Done</p>
+                    <button className='bg-blue-700 px-4 py-2 mt-2 rounded hover:bg-blue-800'>Download Resume</button>
                 </div>
 
-                {/* <div className="flex flex-col items-center gap-2 text-center">
-                    <FaCheckCircle size={24} />
-                    <h3 className='text-lg font-bold'>+2</h3>
-                    <p>Happy Clients</p>
-                </div> */}
+                <div className="flex justify-around w-full p-4 z-10">
 
-                <div className="flex flex-col items-center gap-2 text-center">
-                    <FaCodeBranch size={24} />
-                    <h3 className='text-lg font-bold'>+2000</h3>
-                    <p>Code Commits</p>
+                    <div className="flex flex-col items-center gap-2 text-center">
+                        <FaCheckCircle size={24} />
+                        <h3 className='text-lg font-bold'>8+</h3>
+                        <p>Projects Done</p>
+                    </div>
+
+                    {/* <div className="flex flex-col items-center gap-2 text-center">
+                        <FaCheckCircle size={24} />
+                        <h3 className='text-lg font-bold'>+2</h3>
+                        <p>Happy Clients</p>
+                    </div> */}
+
+                    <div className="flex flex-col items-center gap-2 text-center">
+                        <FaCodeBranch size={24} />
+                        <h3 className='text-lg font-bold'>2000+</h3>
+                        <p>Code Commits</p>
+                    </div>
                 </div>
+
             </div>
+
+            {/* Background Image */}
+            {/* <div className="background-image-container pointer-events-none">
+                <ProgressiveImage src={bgColorImg} placeholder={''}>
+                    {(src, loading) => (
+                        <img
+                            src={src}
+                            alt={""}
+                            className={`w-full h-full object-cover duration-300 ${loading ? 'opacity-0' : 'opacity-100'}`}
+                        />
+                    )}
+                </ProgressiveImage>
+            </div> */}
+
         </div>
     </SectionWrapper>
   )
