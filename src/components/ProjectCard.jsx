3108dace6658e27dfb3abbe1247b30d3518a10af
Vonglory176 <SkylerGConley@gmail.com>
2024-08-25 19:56:28 -0700

    Fixed some formatting issues and updated image pathing

--- src/components/ProjectCard.jsx
+++ src/components/ProjectCard.jsx
@@ -8,9 +8,9 @@ const ProjectCard = ({project, index}) => {
     const { showModal } = useSiteContext()
 
     return (
-        <div key={index} className='project-card bg-gray-800 rounded-lg overflow-hidden relative w-[343px] h-[195px]'>
+        <div key={index} className='project-card bg-gray-800 rounded-lg overflow-hidden relative w-full h-full max-w-[384px] max-h-[216px] shadow-lg'> {/* w-[343px] h-[195px] */}
 
-            <ProgressiveImage src={project.thumbnail} placeholder={project.thumbnail}>
+            <ProgressiveImage src={project.thumbnail.full} placeholder={project.thumbnail.small}>
                 {(src, loading) => (
                     <img
                         src={src}
@@ -21,11 +21,11 @@ const ProjectCard = ({project, index}) => {
             </ProgressiveImage>
             {/* <img src={project.thumbnail} alt={project.name} className='project-card-image w-full h-full object-cover' /> */}
 
-            <div className='project-card-info flex flex-col items-center justify-between text-center gap-2 p-4 absolute bottom-0 bg-black bg-opacity-40 w-full h-full'>
+            <div className='project-card-info flex flex-col items-center justify-between text-center gap-2 py-4 absolute bottom-0 bg-black bg-opacity-50 w-full h-full'>
                 
                 {/* Project Name and Description */}
-                <div className='flex flex-col items-center justify-center gap-2'>
-                    <h3 className='text-xl font-bold'>{project.name}</h3>
+                <div className='flex flex-col items-center justify-center gap-2 px-4'>
+                    <h3 className='text-xl font-bold text-blue-400'>{project.name}</h3>
                     <p>{project.description}</p>
                 </div>
 
@@ -33,7 +33,7 @@ const ProjectCard = ({project, index}) => {
                 <TextCarosuel skills={project.skills} />
 
                 {/* Buttons */}
-                <div className='flex items-center justify-between gap-2 w-full'>
+                <div className='flex items-center justify-between gap-2 w-full px-4'>
                     <a href={project.link} target='_blank' className='bg-blue-600 text-white p-3 rounded-full'><FaExternalLinkAlt size={20} /></a>
                     <a href={project.github} target='_blank' className='bg-blue-600 text-white p-3 rounded-full'><FaGithub size={20} /></a>
                     <button onClick={() => showModal(project)} className='bg-blue-600 text-white p-3 rounded-full flex items-center gap-2'>
