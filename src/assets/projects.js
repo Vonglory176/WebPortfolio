4688cd632adf9f2b827aa5dfe74dac530b2d4321
Vonglory176 <SkylerGConley@gmail.com>
2024-08-25 19:56:53 -0700

    Added another project with serveral dummy duplicates. Also changed the pathing for images

--- src/assets/projects.js
+++ src/assets/projects.js
@@ -1,17 +1,170 @@
+// Gym App
 import gymAppImage1 from '../assets/images/projects/gymApp/gym-app-1.webp'
 import gymAppImage2 from '../assets/images/projects/gymApp/gym-app-2.webp'
+import gymAppImage3 from '../assets/images/projects/gymApp/gym-app-3.webp'
+import gymAppImage1Small from '../assets/images/projects/gymApp/gym-app-1-small.webp'
+import gymAppImage2Small from '../assets/images/projects/gymApp/gym-app-2-small.webp'
+import gymAppImage3Small from '../assets/images/projects/gymApp/gym-app-3-small.webp'
 import gymAppThumbnail from '../assets/images/projects/gymApp/gym-app-thumbnail.webp'
+
+// FreeScribe
+import freeScribeImage1 from '../assets/images/projects/freeScribe/freeScribe-1.webp'
+import freeScribeImage2 from '../assets/images/projects/freeScribe/freeScribe-2.webp'
+import freeScribeImage3 from '../assets/images/projects/freeScribe/freeScribe-3.webp'
+import freeScribeImage4 from '../assets/images/projects/freeScribe/freeScribe-4.webp'
+import freeScribeImage1Small from '../assets/images/projects/freeScribe/freeScribe-1-small.webp'
+import freeScribeImage2Small from '../assets/images/projects/freeScribe/freeScribe-2-small.webp'
+import freeScribeImage3Small from '../assets/images/projects/freeScribe/freeScribe-3-small.webp'
+import freeScribeImage4Small from '../assets/images/projects/freeScribe/freeScribe-4-small.webp'
+import freeScribeThumbnail from '../assets/images/projects/freeScribe/freeScribe-thumbnail.webp'
+
 export const projects = [
     {
         name: 'Gym App',
         description: 'A website for building workout plans',
-        thumbnail: gymAppThumbnail,
-        images: [
-            gymAppImage1,
-            gymAppImage2,
-        ],
-        link: 'https://www.example.com/project1',
-        github: 'https://www.example.com/project1',
+        thumbnail: {
+            full: gymAppThumbnail,
+            small: gymAppThumbnail,
+        },
+        images: {
+            full: [
+                gymAppImage1,
+                gymAppImage2,
+                gymAppImage3,
+            ],
+            small: [
+                gymAppImage1Small,
+                gymAppImage2Small,
+                gymAppImage3Small,
+            ],
+        },
+        link: 'https://skylers-gym-trainer.netlify.app/',
+        github: 'https://github.com/Vonglory176/Gym-App',
         skills: ['HTML', 'CSS', 'Tailwind', 'TypeScript', 'React', 'Vite'],
+    },
+    {
+        name: 'FreeScribe',
+        description: 'A website for recording and transcribing audio',
+        // description: 'A website to record & transcribe audio',
+        thumbnail: {
+            full: freeScribeThumbnail,
+            small: freeScribeThumbnail,
+        },
+        images: {
+            full: [
+                freeScribeImage1,
+                freeScribeImage2,
+                freeScribeImage3,
+                freeScribeImage4,
+            ],
+            small: [
+                freeScribeImage1Small,
+                freeScribeImage2Small,
+                freeScribeImage3Small,
+                freeScribeImage4Small,
+            ],
+        },
+        link: 'https://skylers-transcriber.netlify.app/',
+        github: 'https://github.com/Vonglory176/FreeScribe',
+        skills: ['HTML', 'CSS', 'Tailwind', 'TypeScript', 'React', 'Vite', 'RESTful API'],
+    },
+
+
+    // DUMMY PROJECTS -- DELETE AFTER TESTING !!!!!!
+    {
+        name: 'Gym App',
+        description: 'A website for building workout plans',
+        thumbnail: {
+            full: gymAppThumbnail,
+            small: gymAppThumbnail,
+        },
+        images: {
+            full: [
+                gymAppImage1,
+                gymAppImage2,
+                gymAppImage3,
+            ],
+            small: [
+                gymAppImage1Small,
+                gymAppImage2Small,
+                gymAppImage3Small,
+            ],
+        },
+        link: 'https://skylers-gym-trainer.netlify.app/',
+        github: 'https://github.com/Vonglory176/Gym-App',
+        skills: ['HTML', 'CSS', 'Tailwind', 'TypeScript', 'React', 'Vite'],
+    },
+    {
+        name: 'FreeScribe',
+        description: 'A website to record & transcribe audio',
+        thumbnail: {
+            full: freeScribeThumbnail,
+            small: freeScribeThumbnail,
+        },
+        images: {
+            full: [
+                freeScribeImage1,
+                freeScribeImage2,
+                freeScribeImage3,
+                freeScribeImage4,
+            ],
+            small: [
+                freeScribeImage1Small,
+                freeScribeImage2Small,
+                freeScribeImage3Small,
+                freeScribeImage4Small,
+            ],
+        },
+        link: 'https://skylers-transcriber.netlify.app/',
+        github: 'https://github.com/Vonglory176/FreeScribe',
+        skills: ['HTML', 'CSS', 'Tailwind', 'TypeScript', 'React', 'Vite', 'RESTful API'],
+    },
+    {
+        name: 'Gym App',
+        description: 'A website for building workout plans',
+        thumbnail: {
+            full: gymAppThumbnail,
+            small: gymAppThumbnail,
+        },
+        images: {
+            full: [
+                gymAppImage1,
+                gymAppImage2,
+                gymAppImage3,
+            ],
+            small: [
+                gymAppImage1Small,
+                gymAppImage2Small,
+                gymAppImage3Small,
+            ],
+        },
+        link: 'https://skylers-gym-trainer.netlify.app/',
+        github: 'https://github.com/Vonglory176/Gym-App',
+        skills: ['HTML', 'CSS', 'Tailwind', 'TypeScript', 'React', 'Vite'],
+    },
+    {
+        name: 'FreeScribe',
+        description: 'A website to record & transcribe audio',
+        thumbnail: {
+            full: freeScribeThumbnail,
+            small: freeScribeThumbnail,
+        },
+        images: {
+            full: [
+                freeScribeImage1,
+                freeScribeImage2,
+                freeScribeImage3,
+                freeScribeImage4,
+            ],
+            small: [
+                freeScribeImage1Small,
+                freeScribeImage2Small,
+                freeScribeImage3Small,
+                freeScribeImage4Small,
+            ],
+        },
+        link: 'https://skylers-transcriber.netlify.app/',
+        github: 'https://github.com/Vonglory176/FreeScribe',
+        skills: ['HTML', 'CSS', 'Tailwind', 'TypeScript', 'React', 'Vite', 'RESTful API'],
     },
 ]
