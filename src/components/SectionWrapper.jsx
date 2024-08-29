8ab0e9e5a1e7005396c4852edebf2d876b2ec556
Vonglory176 <SkylerGConley@gmail.com>
2024-08-28 19:31:11 -0700

    Moved all background and scroll position related code into this file

--- src/components/SectionWrapper.jsx
+++ src/components/SectionWrapper.jsx
@@ -1,8 +1,25 @@
 import React from 'react'
+import { useSiteContext } from '../context/SiteContext'
+import { useInView } from 'react-intersection-observer'
+import ProgressiveImage from 'react-progressive-graceful-image'
+
+import bgPattern from '../assets/images/bg-site.png'
+import landingBgColorImg from '../assets/images/bg-colors-5.png'
+import projectsBgColorImg from '../assets/images/bg-colors-6.png'
+import referencesBgColorImg from '../assets/images/bg-colors-3.png'
 
 const SectionWrapper = ({id, title, children}) => {
+  const { handleInView } = useSiteContext()
+  const { ref } = useInView({
+      threshold: 0.6,
+      onChange: handleInView
+  })
+
+  const currentBackgroundImage = id === 'landing' ? landingBgColorImg : id === 'projects' ? projectsBgColorImg : id === 'references' ? referencesBgColorImg : null
+
   return (
-    <section id={id} className='flex flex-col min-h-screen w-full'> {/* min-h-screen */}
+    <section id={id} className='flex flex-col min-h-screen w-full relative' ref={ref}> {/*  style={{backgroundImage: 'url("https://www.transparenttextures.com/patterns/always-grey.png")'}} */}
+    {/* <section id={id} className='flex flex-col min-h-screen w-full' ref={ref} style={{backgroundImage: `url(${bgPattern})`, backgroundOpacity: '0.5'}}> */}
 
         {/* Section Header */}
         {title && <div className='bg-gray-800 bg-opacity-80 sticky top-0 z-20 shadow-sm'>
@@ -10,7 +27,56 @@ const SectionWrapper = ({id, title, children}) => {
         </div>}
 
         {/* Section Content */}
-        {children}
+        <div className='section-content-wrapper flex flex-1 flex-col px-4 py-8 items-center justify-center gap-4 bg-gray-800 bg-opacity-30 relative z-10'>
+          {children}
+        </div>
+
+        {/* -- BACKGROUND IMAGES -- */}
+
+        {/* Landing */}
+        <div className="background-image-container pointer-events-none">
+            {currentBackgroundImage ? 
+              <ProgressiveImage src={currentBackgroundImage} placeholder={''}>
+                {(src, loading) => (
+                    <img
+                        src={src}
+                        alt={""}
+                        className={`w-full h-full object-cover duration-300 ${loading ? 'opacity-0' : 'opacity-100'}`}
+                    />
+                )}
+              </ProgressiveImage>
+
+              :
+
+              null
+            }
+        </div>
+
+        {/* Projects */}
+        {/* <div className="background-image-container pointer-events-none">
+            <ProgressiveImage src={bgColorImg} placeholder={''}>
+                {(src, loading) => (
+                    <img
+                        src={src}
+                        alt={""}
+                        className={`w-full h-full object-cover duration-300 ${loading ? 'opacity-0' : 'opacity-100'}`}
+                    />
+                )}
+            </ProgressiveImage>
+        </div> */}
+
+        {/* References */}
+        {/* <div className="background-image-container pointer-events-none">
+            <ProgressiveImage src={bgColorImg} placeholder={''}>
+                {(src, loading) => (
+                    <img
+                        src={src}
+                        alt={""}
+                        className={`w-full h-auto object-cover object-center duration-300 ${loading ? 'opacity-0' : 'opacity-100'}`}
+                    />
+                )}
+            </ProgressiveImage>
+        </div> */}
 
     </section>
   )
