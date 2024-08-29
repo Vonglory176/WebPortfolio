3317d06c4fe9f985a7efeb5750fb8ead8b416b8d
Vonglory176 <SkylerGConley@gmail.com>
2024-08-28 19:30:05 -0700

    Duration and text hover color tweaks

--- src/components/ProjectCard.jsx
+++ src/components/ProjectCard.jsx
@@ -8,7 +8,7 @@ const ProjectCard = ({project, index}) => {
     const { showModal } = useSiteContext()
 
     return (
-        <div key={index} className='project-card bg-gray-800 rounded-lg overflow-hidden relative w-full h-full max-w-[384px] max-h-[216px] shadow-lg'> {/* w-[343px] h-[195px] */}
+        <div key={index} className='project-card bg-gray-800 rounded-lg overflow-hidden relative w-full h-full max-w-[384px] max-h-[216px] shadow-lg hover:scale-105 duration-300'> {/* w-[343px] h-[195px] // hover:scale-105 */}
 
             <ProgressiveImage src={project.thumbnail.full} placeholder={project.thumbnail.small}>
                 {(src, loading) => (
@@ -21,7 +21,7 @@ const ProjectCard = ({project, index}) => {
             </ProgressiveImage>
             {/* <img src={project.thumbnail} alt={project.name} className='project-card-image w-full h-full object-cover' /> */}
 
-            <div className='project-card-info flex flex-col items-center justify-between text-center gap-2 py-4 absolute bottom-0 bg-black bg-opacity-50 w-full h-full'>
+            <div className='project-card-info flex flex-col items-center justify-between text-center gap-2 py-4 absolute bottom-0 bg-black bg-opacity-50 w-full h-full' style={{ transform: 'scale(1.05)', backfaceVisibility: 'hidden' }}>
                 
                 {/* Project Name and Description */}
                 <div className='flex flex-col items-center justify-center gap-2 px-4'>
@@ -34,9 +34,9 @@ const ProjectCard = ({project, index}) => {
 
                 {/* Buttons */}
                 <div className='flex items-center justify-between gap-2 w-full px-4'>
-                    <a href={project.link} target='_blank' className='bg-blue-600 text-white p-3 rounded-full'><FaExternalLinkAlt size={20} /></a>
-                    <a href={project.github} target='_blank' className='bg-blue-600 text-white p-3 rounded-full'><FaGithub size={20} /></a>
-                    <button onClick={() => showModal(project)} className='bg-blue-600 text-white p-3 rounded-full flex items-center gap-2'>
+                    <a href={project.link} target='_blank' className='bg-blue-600 text-white p-3 rounded-full hover:bg-blue-700 duration-300'><FaExternalLinkAlt size={20} /></a>
+                    <a href={project.github} target='_blank' className='bg-blue-600 text-white p-3 rounded-full hover:bg-blue-700 duration-300'><FaGithub size={20} /></a>
+                    <button onClick={() => showModal(project)} className='bg-blue-600 text-white p-3 rounded-full flex items-center gap-2 hover:bg-blue-700 duration-300'>
                         <FaImages size={20} />
                         {/* <FaTools size={20} /> */}
                         {/* <FaInfo size={20} /> */}
