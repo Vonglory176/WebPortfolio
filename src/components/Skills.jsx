a1981f91bb4493ea03f4bcdbccecf07aedc813a1
Vonglory176 <SkylerGConley@gmail.com>
2024-08-28 19:32:14 -0700

    Button color and grid column tweaks

--- src/components/Skills.jsx
+++ src/components/Skills.jsx
@@ -14,24 +14,24 @@ const Skills = () => {
 
     return (
         <SectionWrapper id='skills' title='Skills'>
-            <div className='flex flex-col flex-1 items-center px-4 py-8 justify-center gap-4 bg-gray-800 bg-opacity-30 relative'> {/* flex-1 h-full  */}
+            {/* <div className='flex flex-col flex-1 items-center px-4 py-8 justify-center gap-4 bg-gray-800 bg-opacity-30 relative'> flex-1 h-full  */}
 
-                <div className="flex flex-col items-center gap-6 px-4 py-6 rounded-lg bg-gray-800 h-[646px] w-full max-w-[500px] shadow-lg">
+                <div className="flex flex-col items-center gap-6 px-4 py-6 rounded-lg bg-gray-800 h-[646px] w-full max-w-[500px] md:max-w-[700px] shadow-lg">
 
                     {/* Skill Selector */}
                     <div className="grid grid-cols-3 gap-5 justify-around w-full">
-                        <button name='frontend' title='Frontend Skills' onClick={() => handleSkillChange('frontend')} className={`flex justify-center ${currentSkill === 'frontend' ? 'bg-blue-900' : 'bg-blue-700'} p-2 duration-200 rounded hover:bg-blue-800 items-center gap-2`}>
+                        <button name='frontend' title='Frontend Skills' onClick={() => handleSkillChange('frontend')} className={`flex justify-center ${currentSkill === 'frontend' ? 'bg-blue-900' : 'bg-blue-700'} p-2 duration-300 rounded hover:bg-blue-800 items-center gap-2`}>
                             <FaCode size={24} /> 
                             <p className="hidden sm:inline-block">Frontend</p>
                         </button>
-                        <button name='backend' title='Backend Skills' onClick={() => handleSkillChange('backend')} className={`flex justify-center ${currentSkill === 'backend' ? 'bg-blue-900' : 'bg-blue-700'} p-2 duration-200 rounded hover:bg-blue-800 items-center gap-2`}>
+                        <button name='backend' title='Backend Skills' onClick={() => handleSkillChange('backend')} className={`flex justify-center ${currentSkill === 'backend' ? 'bg-blue-900' : 'bg-blue-700'} p-2 duration-300 rounded hover:bg-blue-800 items-center gap-2`}>
                             <FaServer size={24} /> 
                             <p className="hidden sm:inline-block">Backend</p>
                         </button>
-                        {/* <button name='design' onClick={handleSkillChange} className='bg-blue-700 p-2 duration-200 rounded hover:bg-blue-800 flex items-center gap-2'>
+                        {/* <button name='design' onClick={handleSkillChange} className='bg-blue-700 p-2 duration-300 rounded hover:bg-blue-800 flex items-center gap-2'>
                             <FaPaintBrush /> Design
                         </button> */}
-                        <button name='other' title='Other Skills' onClick={() => handleSkillChange('other')} className={`flex justify-center ${currentSkill === 'other' ? 'bg-blue-900' : 'bg-blue-700'} p-2 duration-200 rounded hover:bg-blue-800 flex items-center gap-2`}>
+                        <button name='other' title='Other Skills' onClick={() => handleSkillChange('other')} className={`flex justify-center ${currentSkill === 'other' ? 'bg-blue-900' : 'bg-blue-700'} p-2 duration-300 rounded hover:bg-blue-800 flex items-center gap-2`}>
                             <FaTools size={24} /> 
                             <p className="hidden sm:inline-block">Other</p>
                         </button>
@@ -40,7 +40,7 @@ const Skills = () => {
                     <hr className='w-full border-gray-700' />
 
                     {/* Skill Printing */}
-                    <div className="grid grid-cols-3 justify-between gap-5 w-full">
+                    <div className="grid grid-cols-3 justify-between gap-5 w-full md:grid-cols-4 md:gap-8">
                         {skills[currentSkill]?.map((skill, index) => (
 
                             <div key={index} className="flex flex-col items-center gap-2" title={skill.description}>
@@ -54,7 +54,7 @@ const Skills = () => {
                     </div>
 
                 </div>
-            </div>
+            {/* </div> */}
         </SectionWrapper>
     )
 }
