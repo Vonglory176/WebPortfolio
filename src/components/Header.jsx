034cfdbeafe2ec46ac96a6c8f17a9b99da93be07
Vonglory176 <SkylerGConley@gmail.com>
2024-08-29 17:32:04 -0700

    Made some changes regarding pointer events

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -3,10 +3,10 @@ import SidebarNav from './SidebarNav'
 
 const Header = () => {
   return (
-    <header className='w-full p-4 fixed z-30'> {/* bg-gray-800 bg-opacity-80 */}
+    <header className='w-full p-4 fixed z-30 pointer-events-none'> {/* bg-gray-800 bg-opacity-80 */}
 
         <div className='flex items-center justify-between'>
-            <h2 className='font-bold text-2xl'>Skyler<span className='text-blue-500'>.</span></h2>
+            <h2 className='font-bold text-2xl pointer-events-auto'><a href="#landing">Skyler<span className='text-blue-500'>.</span></a></h2>
 
             {/* CHANGE LATER */}
             <SidebarNav />
