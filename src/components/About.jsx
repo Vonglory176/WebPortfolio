110e3f09fba429ab168e359892fee0673eccdab0
Vonglory176 <SkylerGConley@gmail.com>
2024-08-29 17:31:23 -0700

    General styling tests and formatting changes

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -1,6 +1,6 @@
 import React, { useEffect, useRef, useState } from 'react'
 import SectionWrapper from './SectionWrapper'
-import { FaCheckCircle, FaCodeBranch } from 'react-icons/fa'
+import { FaCheckCircle, FaCodeBranch, FaDownload } from 'react-icons/fa'
 import bgColorImg from '../assets/images/bg-colors-1.png'
 import ProgressiveImage from 'react-progressive-graceful-image'
 
@@ -8,7 +8,7 @@ const About = () => {
     const hobbyRef = useRef(null)
     const [currentHobby, setCurrentHobby] = useState(0)
     // const hobbies = ['Mountain Biker']
-    const hobbies = ['Developer', 'Mountain Biker', 'Model Maker', 'Gamer Geek', 'Chess Player', 'Dad Humorist']
+    const hobbies = ['Software Developer', 'Mountain Biker', 'Model Maker', 'Gamer Geek', 'Chess Player', 'Dad Humorist']
 
     useEffect(() => {
         const interval = setInterval(() => {
@@ -37,15 +37,16 @@ const About = () => {
 
             <div className="content-wrapper flex flex-col gap-4 max-w-[548px]"> {/* 619px */}
                 
-                <div className=''>
+                <div className='text-center'>
                     {/* <h3 className='text-2xl font-bold text-left m-auto max-w-[250px] md:max-w-[450px]'>Meet Skyler<span className='text-blue-400'>,</span> an avid <span ref={hobbyRef} className='text-blue-400 transition-opacity opacity-0' key={currentHobby}>{hobbies[currentHobby]}</span></h3> */}
                     
-                    <h1 className='text-3xl font-bold text-center'>Meet Skyler<span className='text-blue-500'>.</span></h1>
-                    <p ref={hobbyRef} className='text-xl font-semibold text-center w-full m-auto duration-300 transition-opacity opacity-0 text-blue-500' key={currentHobby}>{hobbies[currentHobby]}</p>
+                    <h1 className='text-4xl font-bold text-center mb-2'>Meet Skyler<span className='text-blue-500'>.</span></h1>
+                    <h2 ref={hobbyRef} className='text-2xl text-center w-full m-auto duration-300 transition-opacity opacity-0 text-blue-500' key={currentHobby}>{hobbies[currentHobby]}</h2>
+                    {/* <p className='text-xl font-semibold text-center w-full m-auto duration-300 transition-opacity opacity-0 text-blue-500'>{hobbies[currentHobby]}</p> */}
                 </div>
                 
 
-                <div className="flex flex-col items-center gap-4 px-4 py-6 rounded-lg bg-gray-800 z-10 shadow-lg">
+                <div className="flex flex-col items-center gap-4 px-4 py-6 rounded-lg bg-gray-800 z-10 shadow-lg border border-solid border-gray-700">
                     
                     {/* <p>Results-oriented and highly motivated <span className='text-blue-500'>full-stack developer</span> with <span className='text-blue-500'>3+ years</span> of experience.</p>
                     <p>Heavily focused on Web-Development with extensive experience working with the <span className='text-blue-500'>MERN tech stack</span>, this including strong familiarity of <span className='text-blue-500'>Responsive Design, Best Practices and W3C Standards</span> among others.</p>
@@ -59,27 +60,32 @@ const About = () => {
                     <p>Results-oriented and highly motivated <span className='text-blue-400'>Full-Stack Developer</span> with <span className='text-blue-400'>3+ years</span> of experience in <span className='text-blue-400'>Web Development</span>, specializing in the <span className='text-blue-400'>MERN tech stack</span>.</p>
                     <p>Proficient in <span className='text-blue-400'>Responsive Design</span>, <span className='text-blue-400'>Best Practices</span>, and <span className='text-blue-400'>W3C Standards</span>. <span className='text-blue-400'>Self-motivated</span>, <span className='text-blue-400'>fast learner</span>, and always eager to tackle new challenges to expand my skillset.</p>
 
-                    <button className='bg-blue-700 px-4 py-2 mt-2 rounded hover:bg-blue-800 duration-300'>Download Resume</button>
+                    {/* <button className='bg-blue-700 px-4 py-2 mt-2 rounded hover:bg-blue-800 duration-300'>Download Resume</button> */}
                 </div>
 
-                <div className="flex justify-around w-full p-4 z-10 text-gray-400">
+                <div className="flex justify-around gap-2 w-full z-10 text-gray-400 text-center">
+                {/* <div className="flex justify-around w-full p-4 z-10 text-gray-400"> */}
 
-                    <div className="flex flex-col items-center justify-center gap-2 text-center">
+                    <div className="flex flex-col items-center justify-center gap-2 w-full max-w-[110px]">
                         <FaCheckCircle size={24} />
                         <h3 className='text-lg font-bold'>8+</h3>
-                        <p>Projects Done</p>
+                        <p>Projects Completed</p>
                     </div>
 
-                    {/* <button className='bg-blue-700 px-4 py-2 mt-2 rounded hover:bg-blue-800 duration-300'>Download Resume</button> */}
+                    <button className='bg-blue-700 text-white px-4 py-3 my-auto rounded hover:bg-blue-800 duration-300 flex items-center gap-2'>
+                        {/* Download */}
+                        <span className='hidden sm:block'>Download</span>
+                        <span>Resume</span>
+                        <FaDownload />
+                    </button>
 
-
-                    {/* <div className="flex flex-col items-center gap-2 text-center">
+                    {/* <div className="flex flex-col items-center gap-2">
                         <FaCheckCircle size={24} />
                         <h3 className='text-lg font-bold'>+2</h3>
                         <p>Happy Clients</p>
                     </div> */}
 
-                    <div className="flex flex-col items-center gap-2 text-center">
+                    <div className="flex flex-col items-center gap-2 w-full max-w-[110px]">
                         <FaCodeBranch size={24} />
                         <h3 className='text-lg font-bold'>2000+</h3>
                         <p>Code Commits</p>
