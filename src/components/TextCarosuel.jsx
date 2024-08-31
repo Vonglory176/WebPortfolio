7fdcaa150902e3cf4ab15e6b0203d2c186af46ff
Vonglory176 <SkylerGConley@gmail.com>
2024-08-30 19:15:35 -0700

    Border and class tweaks for skills

--- src/components/TextCarosuel.jsx
+++ src/components/TextCarosuel.jsx
@@ -3,11 +3,11 @@ import React from 'react'
 const TextCarosuel = ({skills}) => {
 
     const skillsHtml = skills.map((skill, index) => (
-        <span key={index} className='bg-gray-800 text-white px-2 mx-2 rounded-full inline-block'>{skill}</span>
+        <span key={index} className='bg-gray-800 text-white px-2 mx-2 rounded-full inline-block border border-gray-700'>{skill}</span>
     ))
 
     return (
-        <div className='carousel w-full overflow-hidden whitespace-nowrap box-sizing-border mt-[-5px]'> {/* absolute top-1/2 -translate-y-1/2  */}
+        <div className='skills-carousel w-full min-h-[25px] overflow-hidden whitespace-nowrap box-sizing-border mt-[-5px] text-[14px]'> {/* absolute top-1/2 -translate-y-1/2  */}
 
             {/* Using double for a constant loop */}
             <div className='carousel-content'>
