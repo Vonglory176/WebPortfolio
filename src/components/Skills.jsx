b5110930b17e8e09f2a17d1015d8899ee20b973f
Vonglory176 <SkylerGConley@gmail.com>
2024-08-30 19:18:34 -0700

    Aria labels for buttons and timing tweaks to opacity animation

--- src/components/Skills.jsx
+++ src/components/Skills.jsx
@@ -27,20 +27,20 @@ const Skills = () => {
         skillHeaderRef.current.classList.add('opacity-0')
         skillListRef.current.classList.remove('opacity-100')
         skillListRef.current.classList.add('opacity-0')
-        setTimeout(() => setCurrentSkill(index), 250) // Change to useEffect later !!!
+        setTimeout(() => setCurrentSkill(index), 200) // Change to useEffect later !!!
     }
 
     return (
         <SectionWrapper id='skills' title='Skills'>
             {/* <div className='flex flex-col flex-1 items-center px-4 py-8 justify-center gap-4 bg-gray-800 bg-opacity-30 relative'> flex-1 h-full  */}
 
-                <h1 ref={skillHeaderRef} className='text-4xl font-bold capitalize duration-300'><span className='text-blue-500'>{currentSkill}</span> Skills</h1>
+                <h1 ref={skillHeaderRef} className='text-4xl font-bold capitalize duration-200'><span className='text-blue-500'>{currentSkill}</span> Skills</h1>
 
                 <div className="flex flex-col items-center gap-6 px-4 py-6 rounded-lg bg-gray-800 h-[655px] w-full max-w-[500px] md:max-w-[700px] shadow-lg border border-solid border-gray-700">
                     {/* Skill Selector */}
                     <div className="grid grid-cols-3 gap-5 w-full">
                         {skillList.map((skill, index) => (
-                            <button key={index} name={skill} title={`${skill.charAt(0).toUpperCase() + skill.slice(1)} Skills`} onClick={() => handleSkillChange(skill)} className={`flex justify-center border border-solid ${currentSkill === skill ? 'bg-blue-800 border-blue-500' : 'hover:bg-gray-700 hover:text-white border-gray-700 text-gray-400'} px-4 py-3 duration-300 rounded items-center gap-2`}>
+                            <button key={index} aria-label={`${skill.charAt(0).toUpperCase() + skill.slice(1)} Skills`} name={skill} title={`${skill.charAt(0).toUpperCase() + skill.slice(1)} Skills`} onClick={() => handleSkillChange(skill)} className={`flex justify-center border border-solid ${currentSkill === skill ? 'bg-blue-800 border-blue-500' : 'hover:bg-gray-700 hover:text-white border-gray-700 text-gray-400'} px-4 py-3 duration-300 rounded items-center gap-2`}>
                                 {skills[skill]?.image && React.createElement(skills[skill].image, { size: 24 })}
                                 {/* <p className="hidden sm:inline-block">{skill}</p> */}
                             </button>
@@ -55,7 +55,7 @@ const Skills = () => {
 
                             <div key={index} className="flex flex-col items-center gap-2" title={skill.description}>
                                 {skill.image && <skill.image size={32} />}
-                                <h3 className='text-center'>{skill.name}</h3>
+                                <p className='text-center'>{skill.name}</p>
 
                                 {/* <img src={skill?.image} alt={skill.name} className="w-10 h-10" /> */}
                                 {/* <p>{skill.description}</p> */}
