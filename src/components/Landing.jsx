4a182b3be681e9e9841c1d89dcb439f79e361813
Vonglory176 <SkylerGConley@gmail.com>
2024-08-30 19:16:18 -0700

    General cleanup and aria labels

--- src/components/Landing.jsx
+++ src/components/Landing.jsx
@@ -2,13 +2,11 @@ import React from 'react'
 import SectionWrapper from './SectionWrapper'
 import skylerPicture from '../assets/images/people/skylerPicture.webp'
 import { FaGithub, FaLinkedin, FaGitlab, FaEnvelope, FaWhatsapp, FaChevronDown } from 'react-icons/fa'
-import bgColorImg from '../assets/images/bg-colors-5.png'
 import ProgressiveImage from 'react-progressive-graceful-image'
 
 const Landing = () => {
   return (
     <SectionWrapper id='landing'>
-        {/* <div className='flex flex-col items-center justify-center px-4 py-8 flex-1 h-full relative'>             */}
 
             <div className="content-wrapper flex flex-col items-center gap-4 z-10">
 
@@ -23,7 +21,6 @@ const Landing = () => {
                             />
                         )}
                     </ProgressiveImage>
-                    {/* <img src={skylerPicture} alt="Skyler" className='w-full h-full object-cover' /> */}
                 </div>
 
                 {/* Name and Title */}
@@ -44,15 +41,15 @@ const Landing = () => {
                         <a aria-label="linkedin" href="https://www.linkedin.com/in/skyler-g-conley-1b6a33133/" rel="noreferrer" target="_blank"><FaLinkedin size={24} className='text-white hover:text-blue-500 duration-300' /></a> 
                     </li> 
                     <li> 
-                        <a aria-label="send mail" href="mailto:skylergconley@gmail.com" rel="noreferrer" target="_blank"><FaEnvelope size={24} className='text-white hover:text-blue-500 duration-300' /></a> 
+                        <a aria-label="send mail" href={`mailto:${process.env.REACT_APP_SKYLER_EMAIL}`} rel="noreferrer" target="_blank"><FaEnvelope size={24} className='text-white hover:text-blue-500 duration-300' /></a> 
                     </li> 
-                    {/* <li> 
-                        <a aria-label="whatsapp" href="" rel="noreferrer" target="_blank"><FaWhatsapp /></a> 
-                    </li>  */}
+                    <li> 
+                        <a aria-label="send a text" href={`https://wa.me/${process.env.REACT_APP_SKYLER_PHONE}`} rel="noreferrer" target="_blank"><FaWhatsapp size={24} className='text-white hover:text-blue-500 duration-300' /></a> 
+                    </li> 
                 </ul>
 
                 {/* Contact Button */}
-                <button href="#contact" className='bg-blue-700 px-4 py-2 mt-3 rounded hover:bg-blue-800 duration-300'>Contact Me</button>
+                <a href="#contact" className='bg-blue-700 px-4 py-2 mt-3 rounded hover:bg-blue-800 duration-300'>Contact Me</a>
 
 
                 {/* Scroll For More */}
@@ -61,26 +58,9 @@ const Landing = () => {
 
                     <div className="flex justify-center mt-2 mb-4">
                         <FaChevronDown size={24} className="animate-bounce" />
-                        {/* <FaDoubleChevronDown size={24} className="text-blue-500 animate-bounce" /> */}
                     </div>
                 </div>
             </div>
-
-            {/* Background Image */}
-            {/* <div className="background-image-container pointer-events-none">
-                <ProgressiveImage src={bgColorImg} placeholder={''}>
-                    {(src, loading) => (
-                        <img
-                            src={src}
-                            alt={""}
-                            className={`w-full h-full object-cover duration-300 ${loading ? 'opacity-0' : 'opacity-100'}`}
-                        />
-                    )}
-                </ProgressiveImage>
-            </div> */}
-            
-            {/* <div className="absolute top-0 inset-x-0 h-[37.5rem] bg-grid-slate-900/[0.04] bg-top [mask-image:linear-gradient(0deg,transparent,black)] dark:bg-grid-slate-100/[0.03] dark:bg-[center_top_-1px] dark:border-t dark:border-slate-100/5 top-0 xl:top-18 z-100"></div> */}
-        {/* </div> */}
     </SectionWrapper>
   )
 }
