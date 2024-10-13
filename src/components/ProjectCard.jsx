a4e3d6c31d4e7ca1903a1ac991befcb233456f81
Vonglory176 <SkylerGConley@gmail.com>
2024-10-12 17:44:24 -0700

    Finally fixed the fuzzy transition/transform issues. Info background is now attatched to an image wrapper for the thumbnail. Info no longer transforms, only the image/wrapper

--- src/components/ProjectCard.jsx
+++ src/components/ProjectCard.jsx
@@ -8,22 +8,24 @@ const ProjectCard = ({project, index}) => {
     const { showModal } = useSiteContext()
 
     return (
-        <div key={index} className='project-card bg-gray-800 rounded-lg overflow-hidden relative w-full h-full min-h-[195px] aspect-w-16 aspect-h-9 shadow-lg duration-300'> {/* w-[343px] h-[195px] // hover:scale-105 */}
+        <div key={index} className='project-card bg-gray-800 rounded-lg relative w-full h-full min-h-[195px] aspect-w-16 aspect-h-9'> {/* w-[343px] h-[195px] // hover:scale-105 */}
 
             {/* <ProgressiveImage src={project.thumbnail.full} placeholder={project.thumbnail.small}> */}
-            <ProgressiveImage src={project.images.full[0]} placeholder={project.thumbnail.small}>
-                {(src, loading) => (
-                    <img
-                        src={src}
-                        alt={project.name}
-                        className={`w-full h-full object-cover duration-300 ${loading ? 'blur-sm' : 'blur-0'}`}
-                        // style={{ objectFit: 'cover', imageRendering: 'crisp-edges' }}
-                    />
-                )}
-            </ProgressiveImage>
+            <div className="image-wrapper h-full rounded-lg overflow-hidden duration-300 hover:bg-black hover:bg-opacity-50">
+                <ProgressiveImage src={project.images.full[0]} placeholder={project.thumbnail.small}>
+                    {(src, loading) => (
+                        <img
+                            src={src}
+                            alt={project.name}
+                            className={`w-full h-full object-cover rounded-lg shadow-lg ${loading ? 'blur-sm' : 'blur-0'}`}
+                            // style={{ objectFit: 'cover', imageRendering: 'crisp-edges' }}
+                        />
+                    )}
+                </ProgressiveImage>
+            </div>
             {/* <img src={project.thumbnail} alt={project.name} className='project-card-image w-full h-full object-cover' /> */}
 
-            <div className='project-card-info absolute inset-0 bg-black bg-opacity-50 opacity-0 duration-300 flex items-center justify-center'>
+            <div className='project-card-info rounded-lg absolute inset-0 opacity-0 duration-300 flex items-center justify-center'>
                 
                 <div className="project-card-info-wrapper flex flex-col items-center justify-between text-center py-2 w-full h-full max-h-[257px]">
 
