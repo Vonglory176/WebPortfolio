8fb9b32d22d32d9a3af76879ed310c7d6c8bb930
Vonglory176 <SkylerGConley@gmail.com>
2024-12-06 14:49:06 -0800

    Added more attribute options & fixed 'onLoad' issues

--- src/components/ClientImage.jsx
+++ src/components/ClientImage.jsx
@@ -1,16 +1,37 @@
 'use client'
 
+import { useRef } from 'react'
 import Image from 'next/image'
 
-const ClientImage = ({ src, alt, className }) => {
+const ClientImage = ({ src, alt, className, priority = false, placeholder = '' }) => {
+    const imageRef = useRef(null)
+
+    let attributes = {}
+
+    if (priority) attributes.priority = true
+
+    if (placeholder) {
+        attributes.placeholder = 'blur'
+        attributes.blurDataURL = placeholder
+    }
+
     return (
         <Image
+            ref={imageRef}
             src={src}
             alt={alt}
             className={`${className} opacity-0 duration-300`}
-            onLoadingComplete={(image) => {
-                image.classList.remove('opacity-0')
+            // priority={priority}
+            onLoad={() => {
+                if (imageRef.current) {
+                    imageRef.current.classList.remove('opacity-0')
+                }
             }}
+            // onLoadingComplete={(image) => {
+            //     image.classList.remove('opacity-0')
+            // }}
+
+            {...attributes}
         />
     )
 }
