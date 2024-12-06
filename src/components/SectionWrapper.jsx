258b92bd9dd1c6e4d4d6026af5371698bef111fd
Vonglory176 <SkylerGConley@gmail.com>
2024-12-06 14:50:35 -0800

    General cleanup w/ObserverCover use

--- src/components/SectionWrapper.jsx
+++ src/components/SectionWrapper.jsx
@@ -1,18 +1,17 @@
-// 'use client'
+'use client'
 
-// import React from 'react'
 // import { useSiteContext } from '@/context/SiteContext'
 import ClientImage from '@/components/ClientImage'
-// import { useInView } from 'react-intersection-observer'
-// import ProgressiveImage from 'react-progressive-graceful-image'
+import ObserverCover from '@/components/ObserverCover'
 
-// import bgPattern from '../assets/images/bg-site.png'
 import bgColors1 from '@/assets/images/backgrounds/bg-colors-1.png'
-// import bgColors2 from '@/assets/images/backgrounds/bg-colors-2.png'
 import bgColors3 from '@/assets/images/backgrounds/bg-colors-3.png'
-// import bgColors4 from '@/assets/images/backgrounds/bg-colors-4.png'
 import bgColors5 from '@/assets/images/backgrounds/bg-colors-5.png'
 import bgColors6 from '@/assets/images/backgrounds/bg-colors-6.png'
+
+// import bgPattern from '../assets/images/bg-site.png'
+// import bgColors2 from '@/assets/images/backgrounds/bg-colors-2.png'
+// import bgColors4 from '@/assets/images/backgrounds/bg-colors-4.png'
 
 const SectionWrapper = ({id, title, children}) => {
   // const { handleInView } = useSiteContext()
@@ -38,6 +37,8 @@ const SectionWrapper = ({id, title, children}) => {
     //  min-h-screen
     <section id={id} className='flex flex-col w-full relative' ref={ref}> {/*  style={{backgroundImage: 'url("https://www.transparenttextures.com/patterns/always-grey.png")'}} */}
     {/* <section id={id} className='flex flex-col min-h-screen w-full' ref={ref} style={{backgroundImage: `url(${bgPattern})`, backgroundOpacity: '0.5'}}> */}
+
+        <ObserverCover id={id} />
 
         {/* Section Header */}
         {title && <div className='bg-gray-800 bg-opacity-80 sticky top-0 z-20 shadow-sm border-b border-solid border-gray-700'>
