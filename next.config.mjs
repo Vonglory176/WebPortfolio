2cb6a7a91bf9f456d910d97bd92ea2c8e54c9127
Vonglory176 <SkylerGConley@gmail.com>
2024-12-06 16:52:04 -0800

    Setup for deployment

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,18 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+// const nextConfig = {
+//     // basePath: '/webportfolio', // Not needed for custom domain
+//     output: 'export',
+//     // reactStrictMode: false,
+// };
+
+const nextConfig = {
+    // reactStrictMode: true,
+    // images: {
+    //     unoptimized: true // Disable default image optimization
+    // },
+    assetPrefix: '', // isProd ? '/your-repository-name/' : '',
+    basePath: '', // isProd ? '/your-repository-name' : '',
+    output: 'export'
+}
 
 export default nextConfig;
