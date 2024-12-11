76372583a5913943859111ea1590e124ab04c547
Vonglory176 <SkylerGConley@gmail.com>
2024-12-11 14:42:08 -0800

    Removed "not-found"

--- src/app/robots.js
+++ src/app/robots.js
@@ -10,7 +10,7 @@ export default function robots() {
       {
         userAgent: "*",
         allow: "/",
-        disallow: ["/admin", "/privacy", "/not-found"],
+        disallow: ["/admin", "/privacy"], // , "/not-found"
       },
     ],
     sitemap: `${process.env.NEXT_PUBLIC_BASE_URL}/sitemap.xml`,
